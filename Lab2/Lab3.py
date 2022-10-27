183ba964abff7fa6de5e407d6e46a05cc97e50f1
Damian3002 <damian.makusak500@onet.pl>
2022-10-27 13:15:55 +0200

    Zadania z laboratorium3

---
+++ Lab2/Lab3.py
@@ -0,0 +1,52 @@
+# 1
+# x = int(input("Podaj pierwszą liczbę: "))
+# y = int(input("Podaj drugą liczbę: "))
+# if y < x:
+#     x,y = y,x
+# while x <= y:
+#     print(x, end=" ")
+#     x += 1
+
+
+# 2
+
+# x = -4
+# while x <= 4:
+#     y = 2*x**2 -5*x - 8
+#     print(f"f({x}) = {y}")
+#     x = x + 0.5
+
+# 3
+# while True:
+#     liczba = int(input("podaj liczbe całkowitą: "))
+#     if liczba < 0:
+#         break
+# print("nie podawaj liczby ujemnej")
+
+# 4
+
+# x = int(input("Podaj pierwszą liczbę: "))
+# y = int(input("Podaj drugą liczbę: "))
+# if y < x:
+#      x,y = y,x
+# while x <= y:
+#      if x % 2 != 0:
+#          x += 1
+#          continue
+#      print(x, end=" ")
+#      x += 1
+
+# 5
+# n = int(input("Podaj liczbę studentów: "))
+# a = 1 # wyświetlanie od 1 studenta
+# c = 0 # podstawianie pod dodawanie punktów
+# while a<=n:
+#     b = int(input(f"proszę podać punkty studenta {a}: "))
+#     c+=b # sumowanie punktów studentów
+#     a+=1 # przechodzenie do kolejnego studenta
+# y = c /n # obliczanie średniej
+# print("Średnia wszystkich studentów",y)
+
+
+
+
