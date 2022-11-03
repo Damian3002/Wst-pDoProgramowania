98b691852609e163cab5e4ebd964da2a5e8c8d1f
Damian3002 <damian.makusak500@onet.pl>
2022-11-03 13:28:01 +0100

    Zadania z Lab4

---
+++ Lab2/Lab4.py
@@ -0,0 +1,48 @@
+# 1
+
+# n = int(input("Proszę podać liczbę studentów: "))
+# a = 1 #wyświetlanie od 1 studenta
+# suma = 0 #podstawienie pod dodawanie punktów
+# while True:
+#      b = int(input(f"Proszę podać punkty studenta {a}: "))
+#      if b<0 or b>100:
+#          continue
+#      suma += b #sumowanie punktów studentów
+#      a += 1 #przechodzenie do kolejnego studenta
+#      if a == n+1:
+#          break
+# y = suma/n #obliczanie średniej
+# print("Średnia wszystkich studentów", round(y,2))
+
+# 2
+
+# for i in range(1,101):
+#     print(i, end=' ')
+# 2.1
+
+# for i in range(100, -1 ,-1 ):
+#     print(i ,end=" ")
+
+# 2.2
+# for i in range(7 ,78):
+#     print(i, end=" ")
+
+# 2.3
+# for i in range(20 ,-1 ,-2):
+#     print(i , end=" ")
+
+# 3
+# x = int(input("podaj liczbę: "))
+# for i in range(x):
+#     for l in range(x):
+#         print("* ", end=" ")
+#     print()
+
+# 4
+# lista_zakupów = ["Jaja" , "Mleko" , "Śmietana", "Mąka" , "CocaCola"]
+# print(lista_zakupów)
+# print(lista_zakupów[0],lista_zakupów[1])
+#
+# lista_zakupów[2] = "Mleko"
+# print(lista_zakupów)
+# print(lista_zakupów[-1] ,lista_zakupów[-2])
